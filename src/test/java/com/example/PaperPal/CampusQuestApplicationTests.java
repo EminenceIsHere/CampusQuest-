3f56cb7065742617040bfc55565300de968a8fe4
Vijay Pratap Singh <rashpratap1793@gmail.com>
2025-01-05 22:24:04 +0530

    Update CampusQuestApplicationTests.java
    Signed-off-by: Vijay Pratap Singh <rashpratap1793@gmail.com>

--- src/test/java/com/example/PaperPal/CampusQuestApplicationTests.java
+++ src/test/java/com/example/PaperPal/CampusQuestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaperPalApplicationTests {
+class CampusQuestApplicationTests {
 
 	@Test
 	void contextLoads() {

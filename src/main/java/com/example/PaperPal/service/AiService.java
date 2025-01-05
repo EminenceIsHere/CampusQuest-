6c48e5121d48926bb5f0492b127a41a96cb03d71
Vijay Pratap Singh <rashpratap1793@gmail.com>
2025-01-05 21:57:10 +0530

    Update AiService.java
    Signed-off-by: Vijay Pratap Singh <rashpratap1793@gmail.com>

--- src/main/java/com/example/PaperPal/service/AiService.java
+++ src/main/java/com/example/PaperPal/service/AiService.java
@@ -1,4 +1,4 @@
-package com.example.PaperPal.service;
+package com.example.CampusQuest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;

267e742219e68f5bf0bb2acc9adecc09e697a12e
Vijay Pratap Singh <rashpratap1793@gmail.com>
2025-01-05 21:41:47 +0530

    Update UserRepository.java
    Signed-off-by: Vijay Pratap Singh <rashpratap1793@gmail.com>

--- src/main/java/com/example/PaperPal/repository/UserRepository.java
+++ src/main/java/com/example/PaperPal/repository/UserRepository.java
@@ -1,7 +1,7 @@
-package com.example.PaperPal.repository;
+package com.example.CampusQuest.repository;
 
 
-import com.example.PaperPal.entity.Users;
+import com.example.CampusQuest.entity.Users;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
